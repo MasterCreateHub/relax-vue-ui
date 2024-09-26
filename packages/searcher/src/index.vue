5e83cc5b85aa9b499e1a644e81914449f106fb64
muchunhong <3271018951@qq.com>
2024-09-26 15:38:28 +0800

    perf：优化searcher组件的布局

--- packages/searcher/src/index.vue
+++ packages/searcher/src/index.vue
@@ -1,51 +1,25 @@
 <template>
   <div v-measure="onResize" class="re-searcher">
-    <el-form
-      :class="[{ 'is-justify': labelPosition === 'justify' }]"
-      :model="form"
-      :label-position="labelPosition"
-      v-bind="formConfig"
-      @keyup.enter.native="handleSearch"
-    >
+    <el-form :class="[{ 'is-justify': labelPosition === 'justify' }]" :model="form" :label-position="labelPosition"
+      v-bind="formConfig" @keyup.enter.native="handleSearch">
       <el-row class="re-searcher__body" :gutter="factorSpacing">
-        <el-col
-          class="re-searcher-factor__wrapper"
-          v-for="factor in showFactors"
-          :key="factor.prop"
-          :span="factorSpan"
-        >
+        <el-col :style="{ minWidth: factorMinWidth + 'px' }" class="re-searcher-factor__wrapper"
+          v-for="factor in showFactors" :key="factor.prop" :span="factorSpan">
           <el-form-item :prop="factor.prop" :label="factor.label">
             <slot :name="factor.prop" :form="form">
-              <component
-                :is="factor.type || 'el-input'"
-                v-model="form[factor.prop]"
-                clearable
-                v-bind="factor.config"
-                class="re-factor-item"
-              >
+              <component :is="factor.type || 'el-input'" v-model="form[factor.prop]" clearable v-bind="factor.config"
+                class="re-factor-item">
                 <template v-if="factor.type === 'el-select'">
-                  <el-option
-                    v-for="item in factor.options"
-                    :key="item.value"
-                    :label="item.label"
-                    :value="item.value"
-                  ></el-option>
+                  <el-option v-for="item in factor.options" :key="item.value" :label="item.label"
+                    :value="item.value"></el-option>
                 </template>
                 <template v-if="factor.type === 'el-radio-group'">
-                  <el-radio
-                    v-for="item in factor.options"
-                    :key="item.value"
-                    :label="item.value"
-                  >
+                  <el-radio v-for="item in factor.options" :key="item.value" :label="item.value">
                     {{ item.label }}
                   </el-radio>
                 </template>
                 <template v-if="factor.type === 'el-checkbox-group'">
-                  <el-checkbox
-                    v-for="item in factor.options"
-                    :key="item.value"
-                    :label="item.value"
-                  >
+                  <el-checkbox v-for="item in factor.options" :key="item.value" :label="item.value">
                     {{ item.label }}
                   </el-checkbox>
                 </template>
@@ -53,29 +27,19 @@
             </slot>
           </el-form-item>
         </el-col>
-        <el-col class="re-searcher-action__wrapper" :span="factorSpan">
+        <el-col :style="{ minWidth: factorMinWidth + 'px' }"
+          :class="['re-searcher-action__wrapper', `is-${actionPosition}`]" :span="factorSpan">
           <el-form-item>
             <slot name="action" :form="form">
-              <el-button
-                type="primary"
-                icon="el-icon-search"
-                @click="handleSearch"
-              >
+              <el-button type="primary" icon="el-icon-search" @click="handleSearch">
                 {{ searchText }}
               </el-button>
-              <el-button
-                type="primary"
-                icon="el-icon-refresh"
-                plain
-                @click="handleReset"
-              >
+              <el-button type="primary" icon="el-icon-refresh" plain @click="handleReset">
                 {{ resetText }}
               </el-button>
               <el-button v-if="onlyInline" type="text" @click="handleToggle">
                 {{ expanded ? collapseText : expandText }}
-                <i
-                  :class="expanded ? 'el-icon-arrow-up' : 'el-icon-arrow-down'"
-                ></i>
+                <i :class="expanded ? 'el-icon-arrow-up' : 'el-icon-arrow-down'"></i>
               </el-button>
             </slot>
           </el-form-item>
@@ -110,7 +74,7 @@ export default {
       },
     },
     /**
-     * @description 搜索表单项label位置
+     * @description 搜索条件label位置
      */
     labelPosition: {
       type: String,
@@ -120,18 +84,25 @@ export default {
       },
     },
     /**
-     * @description 是否默认展开
+     * @description 是否默认展开所有搜索条件
      */
     defaultExpand: {
       type: Boolean,
       default: false,
     },
     /**
-     * @description 每个条件的左右间距
+     * @description 搜索条件间距
      */
     factorSpacing: {
       type: Number,
       default: 20,
+    },
+    /**
+     * @description 搜索条件最小宽度
+     */
+    factorMinWidth: {
+      type: Number,
+      default: 240,
     },
     /**
      * @description 搜索条件数组
@@ -189,6 +160,16 @@ export default {
       type: String,
       default: "收起",
     },
+    /**
+     * @description 按钮位置
+     */
+    actionPosition: {
+      type: String,
+      default: "right",
+      validator(value) {
+        return ["left", "right"].includes(value);
+      },
+    },
   },
   data() {
     return {
@@ -200,16 +181,6 @@ export default {
        * @description 视口尺寸
        */
       viewportSize: "lg",
-      /**
-       * @description size配置
-       */
-      sizes: [
-        { label: "xs", value: 12 },
-        { label: "sm", value: 12 },
-        { label: "md", value: 8 },
-        { label: "lg", value: 6 },
-        { label: "xl", value: 4 },
-      ],
     };
   },
   computed: {
@@ -229,10 +200,7 @@ export default {
      */
     showFactors() {
       if (!this.expanded) {
-        let sizeSpan = this.sizes.find(
-          (size) => size.label === this.viewportSize
-        ).value;
-        let inlineMaxNum = (24 / sizeSpan - 1) ? (24 / sizeSpan - 1) : 1;
+        let inlineMaxNum = (24 / this.factorSpan - 1) ? (24 / this.factorSpan - 1) : 1;
         return this.factors.slice(0, inlineMaxNum);
       } else {
         return this.factors;
@@ -242,17 +210,31 @@ export default {
      * @description 搜索条件是否只有一行
      */
     onlyInline() {
-      let sizeSpan = this.sizes.find(
-        (size) => size.label === this.viewportSize
-      ).value;
-      let inlineMaxNum = 24 / sizeSpan - 1;
+      let inlineMaxNum = 24 / this.factorSpan - 1;
       return this.factors.length > inlineMaxNum || this.factors.length === 1;
+    },
+    /**
+     * @description 搜索表单尺寸数组
+     */
+    sizeArray() {
+      let array = [
+        { label: "xxs", number: 1, threshold: 2, span: 24 },
+        { label: "xs", number: 2, threshold: 3, span: 12 },
+        { label: "sm", number: 3, threshold: 4, span: 8 },
+        { label: "md", number: 4, threshold: 6, span: 6 },
+        { label: "lg", number: 6, threshold: 8, span: 4 },
+        { label: "xl", number: 8, threshold: 24, span: 3 },
+      ]
+      return array.map(size => {
+        size.boundary = size.threshold * this.factorMinWidth + (size.threshold - 1) * this.factorSpacing
+        return size
+      })
     },
     /**
      * @description 搜索条件所占栅格的宽度
      */
     factorSpan() {
-      return this.sizes.find((size) => size.label === this.viewportSize).value;
+      return this.sizeArray.find((size) => size.label === this.viewportSize).span;
     },
   },
   mounted() {
@@ -300,16 +282,11 @@ export default {
      */
     onResize(entry) {
       const width = entry.contentRect.width;
-      if (width < 768) {
-        this.viewportSize = "xs";
-      } else if (width < 992) {
-        this.viewportSize = "sm";
-      } else if (width < 1200) {
-        this.viewportSize = "md";
-      } else if (width < 1920) {
-        this.viewportSize = "lg";
-      } else {
-        this.viewportSize = "xl";
+      for (let i = 0; i < this.sizeArray.length; i++) {
+        if (width <= this.sizeArray[i].boundary) {
+          this.viewportSize = this.sizeArray[i].label;
+          break;
+        }
       }
     },
   },
@@ -338,22 +315,25 @@ export default {
     flex-wrap: wrap;
 
     .re-searcher-factor__wrapper {
-      min-width: 240px;
 
       .re-factor-item {
         width: 100%;
       }
     }
 
-    .re-searcher-action__wrapper {
-      min-width: 240px;
+    .is-left {
+      text-align: left;
+    }
+
+    .is-right {
       text-align: right;
       flex: 1;
+    }
 
+    .re-searcher-action__wrapper {
       .el-form-item__content {
         margin-left: 0px !important;
       }
     }
   }
-}
-</style>
+}</style>
