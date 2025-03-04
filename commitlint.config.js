73f431cee0f8fe05a3266daf10bbf760fb6b744e
muchunhong <3271018951@qq.com>
2025-03-04 17:45:24 +0800

    build: 配置commit-msg校验

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'build', 'perf']],
+    'subject-case': [0], // 关闭 subject
+  },
+};
